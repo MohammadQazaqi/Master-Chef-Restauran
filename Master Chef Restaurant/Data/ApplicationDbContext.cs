b14e673ef417e597b0bec07d44ab4fa38c7963e7
Mohammad Qazaqi <mohammedqazaqi5@gmail.com>
2024-09-27 16:34:20 +0300

    Add project files.

---
+++ Master Chef Restaurant/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Master_Chef_Restaurant.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
